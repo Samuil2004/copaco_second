2310ff007c5841dfe6dfe710c4cfff99d1f508be
Samuil2004 <s.kozarov23@acsbg.org>
2024-10-01 10:41:22 +0200

    Component and COmponentType main functionality for searching for all and filtered by type is added

--- CopacoProject/src/main/java/nl/fontys/s3/copacoproject/Controller/ComponentController.java
+++ CopacoProject/src/main/java/nl/fontys/s3/copacoproject/Controller/ComponentController.java
@@ -1,4 +1,36 @@
 package nl.fontys.s3.copacoproject.Controller;
 
+import lombok.AllArgsConstructor;
+import nl.fontys.s3.copacoproject.business.ComponentManager;
+import nl.fontys.s3.copacoproject.business.dto.GetAllComponentsResponse;
+import nl.fontys.s3.copacoproject.business.dto.GetComponentsByCategoryResponse;
+import nl.fontys.s3.copacoproject.persistence.ComponentRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/components")
+@AllArgsConstructor
 public class ComponentController {
+
+    private final ComponentManager componentManager;
+
+
+    @GetMapping("")
+    public ResponseEntity<GetAllComponentsResponse> getAllComponents() {
+
+        GetAllComponentsResponse response = componentManager.getAllComponents();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{category}")
+    public ResponseEntity<GetComponentsByCategoryResponse> getComponentsInCategory(@PathVariable("category") String category) {
+
+        GetComponentsByCategoryResponse response = componentManager.getComponentsByCategory(category);
+        return ResponseEntity.ok(response);
+    }
+
 }
--- CopacoProject/src/main/java/nl/fontys/s3/copacoproject/business/impl/ComponentManagerImpl.java
+++ CopacoProject/src/main/java/nl/fontys/s3/copacoproject/business/impl/ComponentManagerImpl.java
@@ -1,4 +1,26 @@
 package nl.fontys.s3.copacoproject.business.impl;
 
-public class ComponentManagerImpl {
+import lombok.AllArgsConstructor;
+import nl.fontys.s3.copacoproject.business.ComponentManager;
+import nl.fontys.s3.copacoproject.business.dto.GetAllComponentsResponse;
+import nl.fontys.s3.copacoproject.business.dto.GetComponentsByCategoryResponse;
+import nl.fontys.s3.copacoproject.persistence.ComponentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ComponentManagerImpl implements ComponentManager {
+
+    private final ComponentRepository componentRepository;
+
+    @Override
+    public GetAllComponentsResponse getAllComponents() {
+        return GetAllComponentsResponse.builder()
+                .allComponents(componentRepository.getAllComponents().stream().map(ComponentConverter::convertEntityToNormal).toList()).build();
+    }
+
+    @Override
+    public GetComponentsByCategoryResponse getComponentsByCategory(String category) {
+        return GetComponentsByCategoryResponse.builder().allComponentsInCategory(componentRepository.getComponentsByType(category).stream().map(ComponentConverter::convertEntityToNormal).toList()).build();
+    }
 }
