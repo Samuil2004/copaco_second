253c30b5bc84e87c842a867f11dbc67ae8a20bf8
mariucaBianca <540269@student.fontys.nl>
2024-11-04 21:43:10 +0100

    made endpoint for get specification type by componentId

--- CopacoProject/src/main/java/nl/fontys/s3/copacoproject/persistence/SpecificationTypeRepository.java
+++ CopacoProject/src/main/java/nl/fontys/s3/copacoproject/persistence/SpecificationTypeRepository.java
@@ -2,10 +2,15 @@ package nl.fontys.s3.copacoproject.persistence;
 
 import nl.fontys.s3.copacoproject.persistence.entity.SpecificationTypeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SpecificationTypeRepository extends JpaRepository<SpecificationTypeEntity, Long> {
     Optional<SpecificationTypeEntity> findById(Long id);
     SpecificationTypeEntity findBySpecificationTypeName(String specificationTypeName);
+    @Query("SELECT s.specificationType FROM SpecficationTypeList_ComponentTypeEntity s WHERE s.componentType.id = (SELECT c.componentType.id FROM ComponentEntity c WHERE c.componentId = :componentId)")
+    List<SpecificationTypeEntity> findSpecificationTypeEntitiesByComponentId(@Param("componentId") long componentId);
 }
