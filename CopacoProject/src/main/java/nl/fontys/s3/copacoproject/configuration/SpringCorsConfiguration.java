6aeaf9d6f2b31f438c7174486377414577aa72d6
mariucaBianca <540269@student.fontys.nl>
2024-11-18 15:24:04 +0100

    cors config renamed

--- CopacoProject/src/main/java/nl/fontys/s3/copacoproject/configuration/SpringCorsConfiguration.java
+++ CopacoProject/src/main/java/nl/fontys/s3/copacoproject/configuration/SpringCorsConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SprintCrossConfiguration {
+public class SpringCorsConfiguration {
     @Bean
     public WebMvcConfigurer corsConfigurer(@Value("${cors.allowedOrigins}") String[] allowedOrigins) {
         return new WebMvcConfigurer() {
