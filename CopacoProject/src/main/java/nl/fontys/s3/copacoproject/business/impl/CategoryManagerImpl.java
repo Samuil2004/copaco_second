1d888ded6e8243dadce92951def23909d35f45fa
mariucaBianca <540269@student.fontys.nl>
2024-11-30 21:32:09 +0100

    tests for CategoryManagerImpl 100% coverage

--- CopacoProject/src/main/java/nl/fontys/s3/copacoproject/business/impl/CategoryManagerImpl.java
+++ CopacoProject/src/main/java/nl/fontys/s3/copacoproject/business/impl/CategoryManagerImpl.java
@@ -1,39 +1,43 @@
-package nl.fontys.s3.copacoproject.business.impl;
+    package nl.fontys.s3.copacoproject.business.impl;
 
-import lombok.RequiredArgsConstructor;
-import nl.fontys.s3.copacoproject.business.CategoryManager;
-import nl.fontys.s3.copacoproject.business.converters.CategoryConverter;
-import nl.fontys.s3.copacoproject.domain.Category;
-import nl.fontys.s3.copacoproject.persistence.CategoryRepository;
-import nl.fontys.s3.copacoproject.persistence.entity.CategoryEntity;
-import org.springframework.stereotype.Service;
+    import lombok.RequiredArgsConstructor;
+    import nl.fontys.s3.copacoproject.business.CategoryManager;
+    import nl.fontys.s3.copacoproject.business.Exceptions.ObjectNotFound;
+    import nl.fontys.s3.copacoproject.business.converters.CategoryConverter;
+    import nl.fontys.s3.copacoproject.domain.Category;
+    import nl.fontys.s3.copacoproject.persistence.CategoryRepository;
+    import nl.fontys.s3.copacoproject.persistence.entity.CategoryEntity;
+    import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+    import java.util.ArrayList;
+    import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class CategoryManagerImpl implements CategoryManager {
-    private final CategoryRepository categoryRepository;
+    @Service
+    @RequiredArgsConstructor
+    public class CategoryManagerImpl implements CategoryManager {
+        private final CategoryRepository categoryRepository;
 
-    @Override
-    public List<Category> getAllCategories() {
-        List<CategoryEntity> categoryEntities = categoryRepository.findAll();
-        List<Category> categories = new ArrayList<>();
-        if(categoryEntities.size() > 0){
-            for(CategoryEntity categoryEntity : categoryEntities) {
-                categories.add(CategoryConverter.convertFromEntityToBase(categoryEntity));
+        @Override
+        public List<Category> getAllCategories() {
+            List<CategoryEntity> categoryEntities = categoryRepository.findAll();
+            List<Category> categories = new ArrayList<>();
+            if(categoryEntities.size() > 0){
+                for(CategoryEntity categoryEntity : categoryEntities) {
+                    categories.add(CategoryConverter.convertFromEntityToBase(categoryEntity));
+                }
             }
+            return categories;
         }
-        return categories;
-    }
 
-    @Override
-    public Category findCategoryById(long id) {
-        return CategoryConverter.convertFromEntityToBase(categoryRepository.findById(id).orElse(null));
+        @Override
+        public Category findCategoryById(long id) {
+            if(!categoryRepository.existsById(id)) {
+                throw new ObjectNotFound("There is no category with this id");
+            }
+            return CategoryConverter.convertFromEntityToBase(categoryRepository.findById(id).orElse(null));
+        }
+        @Override
+        public CategoryEntity findCategoryByName(String categoryName) {
+            return categoryRepository.findByCategoryName(categoryName);
+        }
     }
-    @Override
-    public CategoryEntity findCategoryByName(String categoryName) {
-        return categoryRepository.findByCategoryName(categoryName);
-    }
-}
