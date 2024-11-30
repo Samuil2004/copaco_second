8530810980cb4587da6585940007fe852576b96e
mariucaBianca <540269@student.fontys.nl>
2024-11-30 20:42:31 +0100

    made endpoint getAllBrands

--- CopacoProject/src/main/java/nl/fontys/s3/copacoproject/business/BrandManager.java
+++ CopacoProject/src/main/java/nl/fontys/s3/copacoproject/business/BrandManager.java
@@ -1,7 +1,10 @@
-package nl.fontys.s3.copacoproject.business.impl;
+package nl.fontys.s3.copacoproject.business;
 
 import nl.fontys.s3.copacoproject.domain.Brand;
 
+import java.util.List;
+
 public interface BrandManager {
     Brand getBrandById(long id);
+    List<Brand> getAllBrands();
 }
