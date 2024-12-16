4e850be367c36765332fdfecb6f2ed17c21945fc
mariucaBianca <540269@student.fontys.nl>
2024-12-16 15:30:38 +0100

    fixed get paginated specification types now it also returns the number of items

--- CopacoProject/src/main/java/nl/fontys/s3/copacoproject/business/SpecificationTypeManager.java
+++ CopacoProject/src/main/java/nl/fontys/s3/copacoproject/business/SpecificationTypeManager.java
@@ -3,6 +3,7 @@ package nl.fontys.s3.copacoproject.business;
 import nl.fontys.s3.copacoproject.business.dto.specificationTypeDto.CreateSpecificationTypeRequest;
 import nl.fontys.s3.copacoproject.business.dto.specificationTypeDto.CreateSpecificationTypeResponse;
 import nl.fontys.s3.copacoproject.business.dto.specificationTypeDto.GetAllSpecificationTypeResponse;
+import nl.fontys.s3.copacoproject.business.dto.specificationTypeDto.GetSpecificationTypeByComponentTypeResponse;
 import nl.fontys.s3.copacoproject.domain.SpecificationType;
 
 import java.util.List;
@@ -13,5 +14,5 @@ public interface SpecificationTypeManager {
     void deleteSpecificationType(Long id);
     SpecificationType getSpecificationType(Long id);
     List<SpecificationType> getSpecificationTypesByComponentId(Long componentId);
-    List<SpecificationType> getSpecificationTypesByComponentTypeId(Long componentTypeId, int currentPage, int itemsPerPage);
+    GetSpecificationTypeByComponentTypeResponse getSpecificationTypesByComponentTypeId(Long componentTypeId, int currentPage, int itemsPerPage);
 }
